ce86bae1e51999ca253f34283bb4221635ab048c
Daniel <danielpalubiak06@gmail.com>
2024-12-18 12:30:53 +0100

    vsetko

--- skola/urls.py
+++ skola/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('studenti/', views.list_students,name="list-student"),
+    path('ucitelia/', views.list_teachers,name="list-student"),
+    path('triedy/', views.list_class,name="list-trieda"),
 ]
